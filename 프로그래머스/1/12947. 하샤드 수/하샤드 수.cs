dcea7522b78028f58b597fbe80d81de3e7a4f032
cucurubingbbong <93061270+cucurubingbbong@users.noreply.github.com>
2026-08-25 23:26:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.15 ms, Memory: 46.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool solution(int x) {
+        int sum = 0;
+        int originalX = x; 
+
+        while (x > 0)
+        {
+            sum += x % 10; 
+            x /= 10;       
+        }
+        return (originalX % sum == 0);
+    }
+}
