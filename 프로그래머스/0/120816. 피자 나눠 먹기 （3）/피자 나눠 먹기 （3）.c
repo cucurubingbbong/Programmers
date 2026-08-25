7733586e21c9db55cd3fa51ea5be782d534350cf
cucurubingbbong <93061270+cucurubingbbong@users.noreply.github.com>
2026-08-25 23:33:42 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int slice, int n) {
+    if ((n%slice)==0)
+    {
+        return n/slice;
+    }
+    else
+        return (n/slice)+1;
+}
